8450c4bac353f1b001c545eb23069ef5c94dee4e
liu.jun <jun.liu@dianping.com>
2016-02-25 17:17:59 +0800

    代码微调

--- src/com/slfuture/pluto/etc/GraphicsHelper.java
+++ src/com/slfuture/pluto/etc/GraphicsHelper.java
@@ -238,4 +238,28 @@ public class GraphicsHelper {
 			return upperBound;
 		}
 	}
+
+    /**
+     * 根据手机的分辨率从 dp 的单位 转成为 px
+     * 
+     * @param context 上下文
+     * @param dpValue DP值
+     * @return 像素值
+     */
+    public static int dip2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /** 
+     * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
+     * 
+     * @param context 上下文
+     * @param pxValue 像素值
+     * @return DP值
+     */
+    public static int px2dip(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
 }
